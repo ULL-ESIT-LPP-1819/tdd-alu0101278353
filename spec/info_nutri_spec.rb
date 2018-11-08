21951498234b3aeb3775da85fe6053f629784f4c
fernanda flores <alu0101278353@ull.edu.es>
2018-11-08 23:16:05 +0000

    creando la preuba para las proteinas

--- spec/info_nutri_spec.rb
+++ spec/info_nutri_spec.rb
@@ -2,7 +2,7 @@ require 'lib/info_nutri/info_nutri'
 
 RSpec.describe Info_nutricional do
         before :each do
-		@valoracion=Info_nutricional.new(3.4,4.6)
+		@valoracion=Info_nutricional.new(3.4,4.6, 3.1)
 	end
 
         it "valoracion de las grasas" do
@@ -13,6 +13,11 @@ RSpec.describe Info_nutricional do
 	it "valoracion de los hidratos de carbono" do
 		expect(@valoracion.vhidrakj.round(2)).to eq(78.2)
 		expect(@valoracion.vhidrakcal).to eq(18.4)
+	end
+
+	it "valoracion de las proteinas" do
+		expect(@valoracion.vprotkj).to eq(52.7)
+		expect(@valoracion.vprotkcal).to eq(12.4)
 	end
 
 end
